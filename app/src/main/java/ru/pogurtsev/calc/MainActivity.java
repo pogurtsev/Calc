de50fc0c94b6f3c9a10ae56e883d9c4c542c9fe7
pogurtsev <76749925+pogurtsev@users.noreply.github.com>
2021-06-09 11:19:22 +0300

    обработка нажатий через onClick (#1)
    * обработка нажатий через onClick
    
    * исправил private и константы

--- app/src/main/java/ru/pogurtsev/calc/MainActivity.java
+++ app/src/main/java/ru/pogurtsev/calc/MainActivity.java
@@ -1,14 +1,107 @@
 package ru.pogurtsev.calc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultField;
+    private TextView numberField;
+    private TextView operationField;
+    private Double operand = null;
+    private String lastOperation = "=";
+    private final static String OPERATION = "OPERATION";
+    private final static String OPERAND = "OPERAND";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultField =(TextView) findViewById(R.id.resultField);
+        numberField = (TextView) findViewById(R.id.numberField);
+        operationField = (TextView) findViewById(R.id.operationField);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle instanceState) {
+        instanceState.putString(OPERATION, lastOperation);
+        if(operand!=null)
+            instanceState.putDouble(OPERAND, operand);
+        super.onSaveInstanceState(instanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle instanceState) {
+        super.onRestoreInstanceState(instanceState);
+        lastOperation = instanceState.getString(OPERATION);
+        operand= instanceState.getDouble(OPERAND);
+        resultField.setText(operand.toString());
+        operationField.setText(lastOperation);
+    }
+
+    public void onNumberClick(View view){
+        Button button = (Button)view;
+        numberField.append(button.getText());
+        if(lastOperation.equals("=") && operand!=null){
+            operand = null;
+            operationField.setText("");
+        }
+    }
+
+    public void onOperationClick(View view){
+        Button button = (Button)view;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+        if(number.length()>0){
+            number = number.replace(',', '.');
+            try{
+                performOperation(Double.valueOf(number), op);
+            }catch (NumberFormatException ex){
+                numberField.setText("");
+            }
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+
+    private void performOperation(Double number, String operation){
+        if(operand ==null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand =0.0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "*":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace('.', ','));
+        numberField.setText("");
     }
 }
